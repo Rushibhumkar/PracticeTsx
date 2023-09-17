8ef1ffdf6b7d42cba6f145750a949c35ca840d97
Rushikesh Jagdevrao Bhumkar <94697002+Rushibhumkar@users.noreply.github.com>
2023-09-17 17:14:42 +0530

    chat app completed

--- chattingapp/screens/Chat.tsx
+++ chattingapp/screens/Chat.tsx
@@ -1,42 +1,44 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState, useCallback} from 'react';
-import {GiftedChat} from 'react-native-gifted-chat';
+import {GiftedChat, InputToolbar, Composer} from 'react-native-gifted-chat';
 import {useRoute} from '@react-navigation/native';
 import firestore from '@react-native-firebase/firestore';
 const Chat = ({data, id}: any) => {
   const route: any = useRoute();
   const [messages, setMessages] = useState([]);
-  console.log(id);
-  console.log(data);
-  console.log(route.params.id + route.params.data.userId);
+  // console.log(id);
+  // console.log(data);
+  // console.log(route.params.id + route.params.data.userId);
   useEffect(() => {
     const subscriber = firestore()
       .collection('chats')
       .doc(route.params.id + route.params.data.userId)
-      .orderBy('createdAt', 'desc');
-    subscriber.onSnapshot((querysnapshot: any) => {
-      const allmessages = querysnapshot.docs.map((item: any) => {
-        return {...item._data, createdAt: Date.parse(new Date())};
+      .collection('messages')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((querysnapshot: any) => {
+        const allmessages = querysnapshot.docs.map((item: any) => {
+          // return {...item._data, createdAt: Date.parse(new Date())};
+          return {...item.data(), createdAt: new Date(item.data().createdAt)};
+        });
+        setMessages(allmessages);
       });
-      setMessages(allmessages);
-    });
     return () => subscriber();
   }, []);
   const onSend = useCallback(async (messages = []) => {
-    const msg = messages[0];
-    const myMsg = {
+    const msg: any = messages[0];
+    const myMsg: any = {
       ...msg,
       sendBy: route.params.id,
       sendTo: route.params.data.userId,
-      createdAt: Date.parse(msg.createdAt),
+      createdAt: msg.createdAt.getTime(),
     };
     setMessages(previousMessages => GiftedChat.append(previousMessages, myMsg));
-    firestore()
+    await firestore()
       .collection('chats')
       .doc('' + route.params.id + route.params.data.userId)
       .collection('messages')
       .add(myMsg);
-    firestore()
+    await firestore()
       .collection('chats')
       .doc('' + route.params.data.userId + route.params.id)
       .collection('messages')
@@ -46,10 +48,18 @@ const Chat = ({data, id}: any) => {
     <View style={{flex: 1}}>
       <GiftedChat
         messages={messages}
-        onSend={messages => onSend(messages)}
+        onSend={(messages: any) => onSend(messages)}
         user={{
           _id: route.params.id,
         }}
+        renderComposer={props => (
+          <Composer
+            {...props}
+            textInputStyle={{
+              color: '#000',
+            }}
+          />
+        )}
       />
     </View>
   );
